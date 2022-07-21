04622c87e18ffeb42a9bd49579e18e5d8cd83023
Lakhan Samani <lakhan.m.samani@gmail.com>
2022-07-21 17:00:27 +0530

    feat: add copy test

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Rclone Test
 
 A sample project to test [rclone](https://rclone.org/) using golang
+
+### Sample Output format of `rclone copy` command
+
+![rclone output](output_format.png)
+
+### Final output
+
+![percentage](final_output.png)
--- main.go
+++ main.go
@@ -1,25 +1,44 @@
 package main
 
 import (
-	"log"
-	"os"
+	"bufio"
+	"fmt"
 	"os/exec"
+	"regexp"
+	"strings"
 )
 
 func main() {
-	// Prepare rclone command
-	c := exec.Command("rclone",
+	cmd := exec.Command("rclone",
 		"--progress",
 		"copy",
-		"src/test_file.txt",
-		"dest/test_file.txt",
-	)
+		// local 2gb file to copy
+		"/Users/lakhansamani/Movies/Archive.zip",
+		"dest/copy_backup.zip")
 
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-
-	err := c.Run()
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		log.Fatal("=> err:", err)
+		fmt.Println(err)
 	}
+
+	err = cmd.Start()
+	fmt.Println("The command is running")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// print the output of the subprocess
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		outputText := scanner.Text()
+		if strings.HasPrefix(outputText, " *") {
+			regex, err := regexp.Compile("\\d+\\%")
+			if err != nil {
+				panic(err)
+			}
+			matches := regex.FindAllString(outputText, -1)
+			fmt.Println("=> Percentage Progress:", matches[1])
+		}
+	}
+	cmd.Wait()
 }
